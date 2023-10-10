0ead6913c4850fe4849762f96a062ed6b89cbd35
Natalia <nataliazanella@yahoo.com.br>
2023-10-10 16:05:32 -0300

    op.aritmetico

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -1,6 +1,31 @@
 public class Operadores {
     public static void main(String [] args){
-        String nomeCompleto = "LINGUAGEM" + "JAVA";
-        System.out.println("É: "+nomeCompleto);
+        String concatenacao = "?";
+
+        concatenacao = 1+1+1+"1";
+
+        System.out.println(concatenacao);
+
+        concatenacao = 1+"1"+1+1;
+
+        System.out.println(concatenacao);
+
+        concatenacao = 1+"1"+1+1;
+
+        System.out.println(concatenacao);
+
+        concatenacao = 1+"1"+1+1+"1";
+
+        System.out.println(concatenacao);
+
+        concatenacao = "1"+1+1+1;
+
+        System.out.println(concatenacao);
+
+        concatenacao = "1"+(1+1+1);
+
+        
+
     }
 }
+
