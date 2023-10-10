701c126a5d594ed1a32d1e826261241aa3513478
Natalia <nataliazanella@yahoo.com.br>
2023-10-10 16:13:17 -0300

    op. aritmetico/atribuição/unario

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -10,3 +10,52 @@ public class Operadores {
        System.out.println(numero);
 
     }
+}
+
+/*  Operadores de atribuição
+
+    public class Operadores {
+    public static void main(String [] args){
+        String nomeCompleto = "LINGUAGEM" + "JAVA";
+        System.out.println("É: "+nomeCompleto);
+    }
+*/
+
+/*  Operadores Aritméticos
+
+public class Operadores {
+    public static void main(String [] args){
+        String concatenacao = "?";
+
+        concatenacao = 1+1+1+"1";
+
+        System.out.println(concatenacao);
+
+        concatenacao = 1+"1"+1+1;
+
+        System.out.println(concatenacao);
+
+        concatenacao = 1+"1"+1+1;
+
+        System.out.println(concatenacao);
+
+        concatenacao = 1+"1"+1+1+"1";
+
+        System.out.println(concatenacao);
+
+        concatenacao = "1"+1+1+1;
+
+        System.out.println(concatenacao);
+
+        concatenacao = "1"+(1+1+1);
+
+        
+
+    }
+}
+
+
+ */
+ 
+
+
