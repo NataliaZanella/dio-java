f5af7bbf88080e64c7c0aee5734b5d1f89f4b3cd
Natalia <nataliazanella@yahoo.com.br>
2023-10-10 18:01:26 -0300

    operadores

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -1,12 +1,18 @@
 public class Operadores {
 
     public static void main(String [] args){
+
+      String nomeUm = "Natalia";
+      String nomeDois = new String("Natalia");
+
+      System.out.println(nomeUm.equals (nomeDois));
+
       int numero1 = 1;
       int numero2 = 2;
 
       boolean simNao = numero1 == numero2;
 
-      if(numero1 < numero2){
+      if(numero1 == numero2){
         System.out.println("a nossa condição é verdadeira");
       }
       
