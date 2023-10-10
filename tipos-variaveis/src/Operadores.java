bd25c89b5c7248ddf4da55f76b4a65d3f3797c10
Natalia <nataliazanella@yahoo.com.br>
2023-10-10 17:51:40 -0300

    operadores

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -1,4 +1,87 @@
 public class Operadores {
+
+    public static void main(String [] args){
+      int numero1 = 1;
+      int numero2 = 2;
+
+      boolean simNao = numero1 == numero2;
+
+      if(numero1 < numero2){
+        System.out.println("a nossa condição é verdadeira");
+      }
+      
+      System.out.println("numeroUm é igual a numeroDois? " + simNao);
+        
+      simNao = numero1 != numero2;
+      
+      System.out.println("numeroUm é diferente a numeroDois? " +simNao);
+      
+        
+      simNao = numero1 > numero2;
+      
+      System.out.println("numeroUm é maior que o numeroDois? " +simNao);
+        
+    }
+}
+
+
+/*
+public class Operadores {
+
+    public static void main(String [] args){
+      int a, b;
+
+      a = 6;
+      b = 6;
+      String resultado = a == b ?"verdadeiro" : "falso";
+      int resultado = a == b ? 1 : 0;    
+      
+      System.out.println(resultado);
+        /*EXEMPLO DE CONDICIONAL UTILIZANDO UMA ESTRUTURA IF/ELSE
+        if(a==b)
+        resultado = "verdadeiro";
+        else
+        resultado = "falso";
+        
+    }
+}
+*/
+
+/*public class Operadores {
+
+    public static void main(String [] args){
+      boolean variavel = true;
+
+      variavel = !variavel;
+
+      System.out.println(variavel);
+
+    }
+}
+*/
+
+/*
+    public class Operadores {
+    public static void main(String [] args){
+      int numero = 5;
+      //x repeticao
+      //numero++;
+      //numero --;
+
+
+      System.out.println(numero ++);
+
+      System.out.println(numero);
+
+    }
+
+}
+*/
+
+
+
+
+/*public class Operadores {
     public static void main(String [] args){
        int numero = 5;
 
@@ -11,6 +94,7 @@ public class Operadores {
 
     }
 }
+*/
 
 /*  Operadores de atribuição
 
