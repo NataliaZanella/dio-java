849e3f6341a9931a2b18b9cb47e37e2bd3937c03
Natalia <nataliazanella@yahoo.com.br>
2023-10-11 16:17:13 -0300

    Escopo

---
+++ tipos-variaveis/src/Conta.java
@@ -0,0 +1,43 @@
+//ESCOPO
+
+// public class Conta {
+    //VARIAVEL DA CLASSE CONTA
+    //double saldo=10.0;
+
+   //public void sacar(Double valor) {
+        //variavel local de método
+        //double novoSaldo = saldo - valor;
+//    }
+
+    /**
+     *
+     */
+    //public void imprimirSaldo(){
+        //disponível em toda classe
+        //System.out.println(saldo);
+      //somente médtodo sacar conhece esta variavel
+       /*  System.out.println(novoSaldo); */
+    
+    //}
+
+    /**
+     * 
+     */
+    //public double calcularDividaExponencial(){
+        //variavel local do metodo
+        //double valorParcela = 50.0;
+       // double valorMontante = 0.0; // começando a variável 
+      //  return valorMontante;
+      //  for(int x=1; x<=5; x++) {
+        //x variável do escopo de bloco
+        //esta variavel será reiniciada a cada execução
+       // double valorCalculado = valorParcela * x;
+       // valorMontante =valorMontante + valorCalculado;
+    
+        //escopo de fluxo
+        //x e valorCalculado nunca estarão disponívelis fora desse bloco
+        
+     //   return valorMontante;
+        
+   // }
+//}
