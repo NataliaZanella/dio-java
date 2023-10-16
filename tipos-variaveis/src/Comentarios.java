9d655a67e7c96803ea4528dce9d7da6d064c791d
Natalia <nataliazanella@yahoo.com.br>
2023-10-16 13:24:12 -0300

    documentacao

---
+++ tipos-variaveis/src/Comentarios.java
@@ -0,0 +1,25 @@
+public class Comentarios {
+    public static void main(String[] args) {
+        //Este é um comentario de uma linha
+
+        /*
+         * Este é um comemntário 
+         * que pode ser mais detalhado
+         * quando necessário
+         */
+
+
+
+    }
+    
+    /**
+     * Esta duas estrelinhas acima é para
+     * identificar que você pretende elaborar um comentário
+     * a nível de documentação.
+     * 
+     */
+
+    public void metodo(){
+
+    }
+}
