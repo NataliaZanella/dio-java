deaebb39fd48710d43fac6e7cd5532bd2f78e3f5
Natalia <nataliazanella@yahoo.com.br>
2023-10-16 15:27:08 -0300

    terminal-argumentos

--- tipos-variaveis/src/Comentarios.java
+++ tipos-variaveis/src/Comentarios.java
@@ -22,4 +22,34 @@ public class Comentarios {
     public void metodo(){
 
     }
+
+
+
+/*
+ * Este método foi elaborado as pressas
+ * por isso eu abrevei o nome das variáveis
+ * mas olha, ele tem a finalidade de somar ou  multiplicar
+ * dois números
+ */
+
+//Um comentário, não possui a finalidade de amenizar um algoritmo não estruturado, conforme as convenções da linguagem.
+public int somaMultiplica (int n, int x, String m){
+    int r = 0; // r é igual ao resultado
+    if (m == "M"){ // M= multiplicação
+        r= n * x;
+    }else{
+        // se não soma mesmo
+        r = n + x;
+    }
+    return r;
 }
+
+
+
+
+
+    
+}
+
+
+
